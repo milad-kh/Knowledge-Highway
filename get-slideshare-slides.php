d194d5d1af310e59d47ae29bad7179b07c207848
khanmohammadi <milad.khanmohammadi@gmail.com>
2018-11-24 01:38:04 +0330

    feature(slideshare): -- get list of slides with a query string

--- get-slideshare-slides.php
+++ get-slideshare-slides.php
@@ -1,23 +1,26 @@
 <?php
-//
-// A very simple PHP example that sends a HTTP POST to a remote site
-//
-
+//user provided settings
+$apikey = '4pvJYcWA';
+$secret = 'Jcs0grSE';
+$ts = time(); //unix timestamp
+$hash = sha1($secret.$ts); //SHA1 (sharedsecret + timestamp)
+$postdata = '&api_key='.$apikey.'&ts='.$ts.'&hash='.$hash.'&q=php'.'&items_per_page=1&page=1';
+//cURL POST call with SSL
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_URL,"https://www.slideshare.net/api/2/search_slideshows");
 curl_setopt($ch, CURLOPT_POST, 0);
-curl_setopt($ch, CURLOPT_POSTFIELDS,
-            "q=javascript");
-
-// In real life you should use something like:
-// curl_setopt($ch, CURLOPT_POSTFIELDS,
-//          http_build_query(array('postvar1' => 'value1')));
-
-// Receive server response ...
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$server_output = curl_exec($ch);
-
-curl_close ($ch);
-var_dump($server_output);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+//execute
+$output = curl_exec($ch);
+ 
+if ($output === FALSE) {
+  echo "cURL Error: " . curl_error($ch);
+} else {
+	print_r($output);
+}
+ 
+curl_close($ch);
+?>
