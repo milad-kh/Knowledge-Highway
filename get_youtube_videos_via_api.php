b2fa4d81268352e0652e3172274495c9b867de50
khanmohammadi <milad.khanmohammadi@gmail.com>
2018-11-23 21:26:55 +0330

    youtube api

---
+++ get_youtube_videos_via_api.php
@@ -0,0 +1,27 @@
+<?php
+error_reporting(E_ALL);
+/**
+ * Library Requirements
+ *
+ * 1. Install composer (https://getcomposer.org)
+ * 2. On the command line, change to this directory (api-samples/php)
+ * 3. Require the google/apiclient library
+ *    $ composer require google/apiclient:~2.0
+ */
+/*if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+  throw new \Exception('please run "composer require google/apiclient:~2.0" in "' . __DIR__ .'"');
+}*/
+echo 'milad';
+require_once __DIR__ . './google-api-php-client-2.2.2/src/Google/autoload.php';
+
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>YouTube Search</title>
+  </head>
+  <body>
+    <?=$htmlBody?>
+  </body>
+</html>
