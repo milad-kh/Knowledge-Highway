40f3e8d52dc495524872c719c97d1eab7483c27e
khanmohammadi <milad.khanmohammadi@gmail.com>
2018-11-24 21:48:57 +0330

    youtube: -- get video list via a keyboard

--- get_youtube_videos_via_api.php
+++ get_youtube_videos_via_api.php
@@ -1,27 +1,17 @@
 <?php
-error_reporting(E_ALL);
-/**
- * Library Requirements
- *
- * 1. Install composer (https://getcomposer.org)
- * 2. On the command line, change to this directory (api-samples/php)
- * 3. Require the google/apiclient library
- *    $ composer require google/apiclient:~2.0
- */
-/*if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
-  throw new \Exception('please run "composer require google/apiclient:~2.0" in "' . __DIR__ .'"');
-}*/
-echo 'milad';
-require_once __DIR__ . './google-api-php-client-2.2.2/src/Google/autoload.php';
-
-?>
-
-<!doctype html>
-<html>
-  <head>
-    <title>YouTube Search</title>
-  </head>
-  <body>
-    <?=$htmlBody?>
-  </body>
-</html>
+$handle = curl_init();
+$url = "https://www.googleapis.com/youtube/v3/search?q=kaveh+afagh&maxResults=25&part=snippet&key=AIzaSyC1VMwaeV9ds1vaGsnayvuBxhtgs0V6NkM";
+// Set the url
+curl_setopt($handle, CURLOPT_URL, $url);
+// Set the result output to be a string.
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+$output = curl_exec($handle);
+curl_close($handle);
+$output = json_decode($output);
+// print_r($output->items);
+foreach ($output->items as $key => $value) {
+	$videoUrl = "https://www.youtube.com/embed/" . $value->id->videoId;
+	?>
+	<iframe width="560" height="315" src="<?php echo($videoUrl); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<?php
+}
